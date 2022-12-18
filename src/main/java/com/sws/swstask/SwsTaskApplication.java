89d72f629242abadd5a1098224afefe8ce624842
Ildar Khaliullin <hali92@list.ru>
2022-12-18 15:45:55 +0300

    Create project

---
+++ src/main/java/com/sws/swstask/SwsTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sws.swstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwsTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwsTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sws/swstask/SwsTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sws.swstask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwsTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
